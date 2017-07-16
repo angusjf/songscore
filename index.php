8c09d121a93d35b50330fbbb197d7ecf5c7dcc1b
Angus Macbook <angus.findlay.mb@gmail.com>
2017-07-16 19:51:58 +0100

    added toHtml methods

--- index.php
+++ index.php
@@ -2,35 +2,27 @@
 	<head>
 		<script src="script.css"></script>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
-		<title>WhatMonitorShouldI[Get/Buy]????</title>
+		<title>SongScore</title>
 		<?php include "classes.php" ?>
 	</head>
 	<body>
 		<div id="container">
 			<div class="section header">
-				<h1>WhatMonitorShouldI[Get/Buy]????</h1>
+				<h1>SongScore</h1>
 			</div>
 			<div class="section">
 				<form>
-					<select name="brand">
-							<option value="dell">Dell</option>
-					</select>
 					<input type="submit">
 				</form>
 			</div>
 			<div class="section">
-				<div id="results">
-					<?php foreach (Monitor::getAll() as $monitor) { ?>
-						<div class="monitor">
-							<img class="monitorImage" src="<?php echo $monitor->image; ?>">
-							<div class="monitorName"><?php echo $monitor->name; ?></div>
-							<div class="monitorPrice"><?php echo "$" . $monitor->price; ?></div>
-						</div>
-					<?php } ?>
+				<div id="reviews">
+					<?php
+					foreach ($userAccount->getFeed(5) as $review) {
+						echo $review->toHtml();
+					}
+					?>
 				</div>
-			</div>
-			<div class="section footer">
-				<p>whatever</p>
 			</div>
 		</div>
 	</body>
