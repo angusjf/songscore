b344719535c14cc5a07c8ae546b606c2300142c4
Angus Macbook <angus.findlay.mb@gmail.com>
2017-07-16 16:29:44 +0100

    added script and html content

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WhatMonitorShouldIBuy
+to start the server on port 80 from the command line
+```sudo php -S localhost:80```
+you only need ```sudo``` if you're using port 80
--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-
+<html>
+	<head>
+		<script src="script.css"></script>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+		<title>WhatMonitorShouldI[Get/Buy]????</title>
+	</head>
+	<body>
+		<div id="container">
+			<div class="section header">
+				<h1>WhatMonitorShouldI[Get/Buy]????</h1>
+			</div>
+			<div class="section">
+				<form>
+				</form>
+			</div>
+			<div class="section">
+				<div id="results">
+			</div>
+			<div class="section footer">
+			</div>
+		</div>
+	</body>
+</html>
