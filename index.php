5f75ad49fbabb5fa385a055d1de017a6c297ca73
Angus Macbook <angus.findlay.mb@gmail.com>
2017-07-16 17:17:51 +0100

    new php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 		<script src="script.css"></script>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 		<title>WhatMonitorShouldI[Get/Buy]????</title>
+		<?php include "classes.php" ?>
 	</head>
 	<body>
 		<div id="container">
@@ -11,12 +12,25 @@
 			</div>
 			<div class="section">
 				<form>
+					<select name="brand">
+							<option value="dell">Dell</option>
+					</select>
+					<input type="submit">
 				</form>
 			</div>
 			<div class="section">
 				<div id="results">
+					<?php foreach (Monitor::getAll() as $monitor) { ?>
+						<div class="monitor">
+							<img class="monitorImage" src="<?php echo $monitor->image; ?>">
+							<div class="monitorName"><?php echo $monitor->name; ?></div>
+							<div class="monitorPrice"><?php echo "$" . $monitor->price; ?></div>
+						</div>
+					<?php } ?>
+				</div>
 			</div>
 			<div class="section footer">
+				<p>whatever</p>
 			</div>
 		</div>
 	</body>
