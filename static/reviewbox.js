feafc8936ad2ecbe75faf131af467dabe36426bb
Angus Macbook <angus.findlay.mb@gmail.com>
2018-04-03 18:01:08 +0100

    started progress on jquery for searching

--- static/reviewbox.js
+++ static/reviewbox.js
@@ -9,6 +9,35 @@ $(document).ready(function () {
 
 		$('#character_count').html(text_remaining + '/' + character_limit);
 	});
+
+	$('#subject-input').keyup(function() {
+		if ($('#expansion-input').val().length > 0) {
+			$.get(
+				"http://ws.audioscrobbler.com/2.0/",
+				{
+					'method' : "track.search",
+					'album' : query,
+					'api_key' : "b392916683d0336a30882ff34ff114f7",
+					'format' : "json",
+					'limit' : 5
+				},
+				data => {
+					/*
+					data.results.albummatches.album.forEach(album => {
+                        results.innerHTML += `
+                                <div>
+                                        <img src='${album.image[0]["#text"]}'/>
+                                        <span style="display: inline-block;">
+                                                <div>${album.name}</div>
+                                                <div>${album.artist}</div>
+										*/
+					// $('#subject-search').add( new html lelements)
+				}
+			);
+		} else {
+			// $('#subject-search').html(); . hide() ?
+		}
+	});
 });
 
 $('#newReview').submit(function (e) {
