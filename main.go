7292119dbcd9b7c8b6c4a5ef56a50ae98822c8d7
angusjf <el17ajf@leeds.ac.uk>
2020-02-04 11:20:02 +0100

    added port thing

--- main.go
+++ main.go
@@ -1,153 +1,163 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
-  "encoding/json"
-  "github.com/gorilla/mux"
-  jwt "github.com/dgrijalva/jwt-go"
-  "github.com/rs/cors"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 type Review struct {
-  Id int `json:"id"`
-  Text string `json:"text"`
-  Stars int `json:"stars"`
-  User User `json:"user"`
-  Subject Subject `json:"subject"`
+	Id      int     `json:"id"`
+	Text    string  `json:"text"`
+	Stars   int     `json:"stars"`
+	User    User    `json:"user"`
+	Subject Subject `json:"subject"`
 }
 
 type User struct {
-  Id int `json:"id"`
-  Username string `json:"username"`
-  Image string `json:"image"`
+	Id       int    `json:"id"`
+	Username string `json:"username"`
+	Image    string `json:"image"`
 }
 
 type Subject struct {
-  Id int `json:"id"`
-  Title string `json:"title"`
-  Artist string `json:"artist"`
-  Image string `json:"image"`
-  Kind SubjectKind `json:"kind"`
+	Id     int         `json:"id"`
+	Title  string      `json:"title"`
+	Artist string      `json:"artist"`
+	Image  string      `json:"image"`
+	Kind   SubjectKind `json:"kind"`
 }
 
 type SubjectKind int
 
 const (
-  Song = 0
-  Album = 1
+	Song  = 0
+	Album = 1
 )
 
 type Credentials struct {
-  Username string `json:"username"`
-  Password string `json:"password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 var jwtSecretKey = []byte("toroymoi")
 
-var me User = User {
-  Id: 0,
-  Username: "angusjf",
-  Image: "",
-};
+var me User = User{
+	Id:       0,
+	Username: "angusjf",
+	Image:    "",
+}
 
-var you Subject = Subject {
-  Id: 0,
-  Title: "You",
-  Artist: "The 1975",
-  Image: "",
-  Kind: Song,
-};
+var you Subject = Subject{
+	Id:     0,
+	Title:  "You",
+	Artist: "The 1975",
+	Image:  "",
+	Kind:   Song,
+}
 
-var exampleReview Review = Review {
-  Id: 1,
-  Text: "listened to it too much :(",
-  Stars: 3,
-  User: me,
-  Subject: you,
-};
+var exampleReview Review = Review{
+	Id:      1,
+	Text:    "listened to it too much :(",
+	Stars:   3,
+	User:    me,
+	Subject: you,
+}
 
-var exampleReviews []Review = []Review {
-  exampleReview,
-};
+var exampleReviews []Review = []Review{
+	exampleReview,
+}
 
 func getReviews(w http.ResponseWriter, r *http.Request) {
-  json.NewEncoder(w).Encode(exampleReviews)
+	json.NewEncoder(w).Encode(exampleReviews)
 }
 
 func getReview(w http.ResponseWriter, r *http.Request) {
-  json.NewEncoder(w).Encode(exampleReview)
+	json.NewEncoder(w).Encode(exampleReview)
 }
 
 func postReview(w http.ResponseWriter, r *http.Request) {
-  var review Review
-  err := json.NewDecoder(r.Body).Decode(&review)
-  if err != nil {
-    fmt.Fprintf(w, "couldn't decode review")
-  }
-  json.NewEncoder(w).Encode(review)
+	var review Review
+	err := json.NewDecoder(r.Body).Decode(&review)
+	if err != nil {
+		fmt.Fprintf(w, "couldn't decode review")
+	}
+	json.NewEncoder(w).Encode(review)
 }
 
 func postAuth(w http.ResponseWriter, r *http.Request) {
-  var credentials Credentials
-  err := json.NewDecoder(r.Body).Decode(&credentials)
-  if err != nil {
-    fmt.Fprintf(w, "couldn't decode")
-    return
-  }
+	var credentials Credentials
+	err := json.NewDecoder(r.Body).Decode(&credentials)
+	if err != nil {
+		fmt.Fprintf(w, "couldn't decode")
+		return
+	}
 
-  token := jwt.New(jwt.SigningMethodHS256)
-  claims := token.Claims.(jwt.MapClaims)
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
 
-  claims["username"] = credentials.Username
+	claims["username"] = credentials.Username
 
-  tokenString, err := token.SignedString(jwtSecretKey)
+	tokenString, err := token.SignedString(jwtSecretKey)
 
-  if err != nil {
-    fmt.Fprintf(w, "couldn't sign token")
-    return
-  }
-  
-  fmt.Fprintf(w, tokenString)
+	if err != nil {
+		fmt.Fprintf(w, "couldn't sign token")
+		return
+	}
+
+	fmt.Fprintf(w, tokenString)
 }
 
 func getMe(w http.ResponseWriter, r *http.Request) {
-  json.NewEncoder(w).Encode(me)
+	json.NewEncoder(w).Encode(me)
 }
 
 func onlyIfAuthorized(endpoint func(w http.ResponseWriter, r *http.Request)) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    if r.Header["Authorization"] != nil {
-      trimmedToken := r.Header.Get("Authorization")[len("Bearer "):]
-      token, err := jwt.Parse(trimmedToken, func(token *jwt.Token) (interface{}, error) {
-        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-          return nil, fmt.Errorf("There was an error") 
-        } else {
-          return jwtSecretKey, nil
-        }
-      })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Authorization"] != nil {
+			trimmedToken := r.Header.Get("Authorization")[len("Bearer "):]
+			token, err := jwt.Parse(trimmedToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				} else {
+					return jwtSecretKey, nil
+				}
+			})
 
-      if err != nil {
-        fmt.Fprintf(w, err.Error())
-      }
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
 
-      if token.Valid {
-        endpoint(w, r)
-      }
-    } else {
-      fmt.Fprintf(w, "Not Authorized: No Auth header")
-    }
-  })
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+			fmt.Fprintf(w, "Not Authorized: No Auth header")
+		}
+	})
 }
 
 func main() {
-  r := mux.NewRouter()
-  r.Handle("/reviews", onlyIfAuthorized(getReviews)).Methods("GET")
-  r.HandleFunc("/reviews/{id}", getReview).Methods("GET")
-  r.HandleFunc("/reviews", postReview).Methods("POST")
-  r.HandleFunc("/auth", postAuth).Methods("POST")
-  r.HandleFunc("/me", getMe).Methods("GET")
-  handler := cors.AllowAll().Handler(r)
-  log.Fatal(http.ListenAndServe(":8081", handler))
+	port := os.Getenv("PORT")
+
+	if port == "" {
+    port = "8081"
+	}
+
+	r := mux.NewRouter()
+	r.Handle("/reviews", onlyIfAuthorized(getReviews)).Methods("GET")
+	r.HandleFunc("/reviews/{id}", getReview).Methods("GET")
+	r.HandleFunc("/reviews", postReview).Methods("POST")
+	r.HandleFunc("/auth", postAuth).Methods("POST")
+	r.HandleFunc("/me", getMe).Methods("GET")
+
+	handler := cors.AllowAll().Handler(r)
+
+	log.Fatal(http.ListenAndServe(":" + port, handler))
 }
